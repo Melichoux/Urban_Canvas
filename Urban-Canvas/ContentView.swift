97cd0d9a866ae176964b06d09e8b6fd35c0335e9
Melissa <meli.tournerie@gmail.com>
2026-07-02 11:43:54 +0200

    Initial Commit

---
+++ Urban-Canvas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Urban-Canvas
+//
+//  Created by Apprenant 72 on 02/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Urban-Canvas/Urban_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Urban_CanvasApp.swift
+//  Urban-Canvas
+//
+//  Created by Apprenant 72 on 02/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Urban_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
